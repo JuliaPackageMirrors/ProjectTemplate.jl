9c4ccee1e2a418928b634d3dd1c2c3b9efd25fe7
John Myles White <jmw@johnmyleswhite.com>
2013-01-06 23:59:18 -0500

    Fixed variable naming and added default config

--- src/create_project.jl
+++ src/create_project.jl
@@ -1,44 +1,58 @@
 function create_project(project_name::String)
-	mkdir("cache")
-	touch(joinpath("cache", ".gitignore"))
+    mkdir("cache")
+    touch(joinpath("cache", ".gitignore"))
 
-	mkdir("config")
-	touch(joinpath("config", "global.json"))
+    mkdir("config")
+    touch(joinpath("config", "global.json"))
+    config = {
+                "recursive_loading" => "on",
+                "load_libraries" => "on",
+                "cache_loading" => "on",
+                "data_loading" => "on",
+                "munging" => "on",
+                "logging" => "off",
+                "libraries" => []
+             }
+    config_text = JSON.to_json(config)
+    config_text = replace(config_text, r",", ",\n")
+    io = open(joinpath("config", "global.json"), "w")
+    println(io, config_text)
+    close(io)
 
-	mkdir("data")
-	touch(joinpath("data", ".gitignore"))
+    mkdir("data")
+    touch(joinpath("data", ".gitignore"))
 
-	mkdir("diagnotics")
-	touch(joinpath("diagnotics", "01.jl"))
+    mkdir("diagnotics")
+    touch(joinpath("diagnotics", "01.jl"))
 
-	mkdir("doc")
-	touch(joinpath("doc", ".gitignore"))
+    mkdir("doc")
+    touch(joinpath("doc", ".gitignore"))
 
-	mkdir("graphs")
-	touch(joinpath("graphs", ".gitignore"))
+    mkdir("graphs")
+    touch(joinpath("graphs", ".gitignore"))
 
-	mkdir("lib")
-	touch(joinpath("lib", "helpers.jl"))
+    mkdir("lib")
+    touch(joinpath("lib", "helpers.jl"))
 
-	mkdir("logs")
-	touch(joinpath("logs", ".gitignore"))
+    mkdir("logs")
+    touch(joinpath("logs", ".gitignore"))
 
-	mkdir("munge")
-	touch(joinpath("munge", "01-A.jl"))
+    mkdir("munge")
+    touch(joinpath("munge", "01-A.jl"))
 
-	mkdir("profiling")
-	touch(joinpath("profiling", "01.jl"))
+    mkdir("profiling")
+    touch(joinpath("profiling", "01.jl"))
 
-	mkdir("reports")
-	touch(joinpath("reports", ".gitignore"))
+    mkdir("reports")
+    touch(joinpath("reports", ".gitignore"))
 
-	mkdir("src")
-	touch(joinpath("src", "EDA.jl"))
+    mkdir("src")
+    touch(joinpath("src", "EDA.jl"))
 
-	mkdir("tests")
-	touch(joinpath("tests", ".gitignore"))
+    mkdir("tests")
+    touch(joinpath("tests", ".gitignore"))
 
-	touch("README.md")
+    touch("README.md")
 
-	touch("TODO.md")
+    touch("TODO.md")
 end
--- src/utils.jl
+++ src/utils.jl
@@ -11,5 +11,8 @@ function clean_variable_name(variable_name::String)
   variable_name = replace(variable_name, r"\\s+", "_")
   variable_name = replace(variable_name, r"\\.+", "_")
   variable_name = replace(variable_name, r"[\\\\/]+", "_")
+  if ismatch(r"^\d", variable_name)
+    variable_name = strcat("x", variable_name)
+  end
   return variable_name
 end
